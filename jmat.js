69082335005f55305a971e4a90a0d4294eb14dab
Jonas Almeida <jalmeida@mathbiol.org>
2011-11-10 03:44:29 -0600

    array to mat and other experiments with ja array vs mat cell type

--- jmat.js
+++ jmat.js
@@ -70,13 +70,68 @@ jmat = {
 		return y;
 	},
 	
-	parse:function(x){
+	parse:function(x){ // x is a stringified Object
 		eval('var res='+x);
 		return res;
 	},
 	
-	gId:function(x){
+	gId:function(x){ // x is the id of an existing DOM element
 		return document.getElementById(x)
+	},
+	
+	unique:function(x){ // x is an Array
+		var u = []; // store unique here
+		u[0]=x[0];
+		for (var i=1; i<x.length; i++){
+			// check if x[i] is new
+			if (u.map(function(ui){return ui===x[i]}).reduce(function(a,b){return a+b})==0){
+				u[u.length]=x[i];
+			}
+		}
+		return u
+	},
+	
+	ranksum:function(x,y){ // this is just a first approximation while something saner emerges for stats
+		var s=x.map(function(xi){return y.map(function(yi){return [yi>xi,yi<xi]}).reduce(function(a,b){return [a[0]+b[0],a[1]+b[1]]})}).reduce(function(a,b){return [a[0]+b[0],a[1]+b[1]]});
+		return Math.abs(s[0]-s[1])/(x.length*y.length);
+	},
+	
+	log:function(x,n){
+		if (!n){return Math.log(x)}
+		else{return Math.log(x)/Math.log(n)}
+	},
+	
+	toBin:function(x,n){
+		if(!n){n=this.log(x,2)+1};
+		var b='';
+		for(i=n-1;i>=0;i=i-1){
+			m=Math.pow(2,i);
+			if(x>=m){b=b+'1';x=x-m}
+			else{b=b+'0'}
+		};
+		return b
+	},
+	
+	cat:function(x,y){ // cat will work for matrices and objects
+		x=this.stringify(x);
+		y=this.stringify(y);
+		return this.parse(x.slice(0,x.length-1)+','+y.slice(1,y.length));		
+	},
+	
+	length:function(x){ // js Array.length returns highest index, not always the numerical length
+		var n=0
+		for(var i in x){n++};
+		return n
+	},
+	
+	array2mat:function(x){ // to handle indexed arrays by converting them into two separate numerically indexed arrays
+		var j = 0, y1=[], y2=[];
+		for(var i in x){
+			y1[j]=i;
+			y2[j]=x[i];
+			j=j+1;
+		}	
+		return [y1,y2]	
 	}
-
+	
 }
