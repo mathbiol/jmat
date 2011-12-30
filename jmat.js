ca70e58d439c4aa29905b90f23cffa2daa4cb0e4
Jonas Almeida <jalmeida@mathbiol.org>
2011-12-30 16:59:15 -0600

    load and read local files (.loadFiles, .readFiles)

--- jmat.js
+++ jmat.js
@@ -156,12 +156,38 @@ length:function(x){ // js Array.length returns highest index, not always the num
 	return n
 },
 
-load:function(url){
+load:function(url){ // load script / JSON
 	var s = document.createElement('script');
 	s.src=url;
 	s.id = this.uid();
 	document.body.appendChild(s);
 	setTimeout('document.body.removeChild(document.getElementById("'+s.id+'"));',30000); // is the waiting still needed ?
+},
+
+loadFiles:function(files,readAs,callback){
+	//<input type="file" id="files" multiple onchange="jmat.loadFiles(this.files,'readAsText')"></input> //<-- example of button for reading text files
+	if(!readAs){readAs='readAsDataURL'} // default is to read as dataURL
+	for(var i=0;i<files.length;i++){
+		this.readFile(files[i],readAs)
+	}
+	return i
+},
+
+readFile:function(f,readAs,callback){
+	var that = this;
+	if(!callback){callback=function(x){ // DEAFAULT CALLBACK - you may want to write your own
+		console.log('---'+x.file.fileName+'---');console.log(x.result); // comment out if you don't want to track reading in the console
+		if(!that.work){that.work={}};if(!that.work.files){that.work.files=[]} // results stored in .work.files
+		that.work.files[x.file.fileName]={file:x.file,result:x.result}; // array indexed to file name
+		}
+	}
+	var reader = new FileReader();
+	reader.onload=(function(theFile){
+		return function(ev){
+			callback({file:theFile,result:ev.target.result})
+		}
+	})(f)
+	reader[readAs](f);
 },
 
 log:function(x,n){
