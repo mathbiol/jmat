f559340c74e3c9a18dde9206f36ab16a366db295
Jonas Almeida <jalmeida@mathbiol.org>
2012-01-24 13:29:44 -0600

    mergingTables

---
+++ mergingTables.js
@@ -0,0 +1,13 @@
+console.log('mergingTables loaded');
+
+mTable={
+	dir:function(){ // list all txt documents
+		//fr=jmat.gId('loadingFrame');
+		
+	},
+	read:function(src){ // read source
+		fr=jmat.gId('loadingFrame');
+		fr.src='.';
+		
+	}
+}
