e50d19a96d158f2849281d3358bc751a6f81a085
Jonas Almeida <jalmeida@uab.edu>
2012-10-08 14:14:26 -0500

    loading 23andme own JS library

--- 23andme.js
+++ 23andme.js
@@ -1,13 +1,15 @@
-//https://api.23andme.com/user/login/?redirect=/authorize/?redirect_uri=http://jmat.googlecode.com/git/23andme.html&response_type&code%26client_id=D09568fd79aa7911dad7727a024936f8d&scope=basic
-
-MB23={ // 23and me application document object model
+//https://api.23andme.com/authorize/?redirect_uri=http://jmat.googlecode.com/git/23andme.html&response_type=code&client_id=09568fd79aa7911dad7727a024936f8d&scope=basic
+// response directed to http://jmat.googlecode.com/git/23andme.html, alternative URL:
+//https://dl-web.dropbox.com/get/http/jmat/23andme.html?w=bba1484b&code={borrow from above}
+//
+UAB={ // 23and me application document object model
 	code:null,
 	parms:{},
 	search4parms:function(){ // gets parameters from call and pushes them to .parms
 		var parms = window.document.location.search.slice(1).split('&');
 		parms.map(function(x){
 			xx = x.split('=');
-			MB23.parms[xx[0]]=xx[1]
+			UAB.parms[xx[0]]=xx[1]
 		})
 
 		//
@@ -17,14 +19,14 @@ MB23={ // 23and me application document object model
 
 
 // find out what is being provided and push it to parms
-MB23.search4parms();
-if(!!MB23.parms.code){
-	MB23.code=MB23.parms.code;
-	console.log('code = '+MB23.code);
+UAB.search4parms();
+if(!!UAB.parms.code){
+	UAB.code=UAB.parms.code;
+	console.log('code = '+UAB.code);
 }
 else if (!!localStorage.getItem('code')){ // find out if there is one in the localstorage
-	MB23.code = localStorage.getItem('code');
-	console.log('code = '+MB23.code);
+	UAB.code = localStorage.getItem('code');
+	console.log('code = '+UAB.code);
 }
 else{
 	throw('23andme access code not found :-(')
